d30ad6ee86c10990b21af9fc2289f0ab668b8692
PHUCMOI <89086140+PHUCMOI@users.noreply.github.com>
2022-10-29 15:03:33 +0700

    Add files via upload

---
+++ src/main/java/com/example/sampleproject/APIClient.java
@@ -0,0 +1,113 @@
+package com.example.sampleproject;
+
+import java.io.IOException;
+import java.security.cert.CertificateException;
+
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+
+
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class APIClient {
+    private static Retrofit retrofit = null;
+    //private static String token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJLcHRXNWJCcTlsRGliY2s5NHI3TldHQVl0SHBrUFI3N1A4V0hMWDVIX1E0In0.eyJleHAiOjE2NjcwOTcyMzksImlhdCI6MTY2NzAxMDg0MCwiYXV0aF90aW1lIjoxNjY3MDEwODM5LCJqdGkiOiJlOTdkZmYyZi1lNDU2LTQ3MmUtOTdlMy02ZjgxNzIzZWMyZDMiLCJpc3MiOiJodHRwczovLzEwMy4xMjYuMTYxLjE5OS9hdXRoL3JlYWxtcy9tYXN0ZXIiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiMTAxZGQ1MmMtMjNiYS00ZjM4LWExMjQtYjc4MGUxYjVhODFiIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoib3BlbnJlbW90ZSIsInNlc3Npb25fc3RhdGUiOiJmNzFlMjVjNi1jOGM4LTRhMTUtODVkYi1lMjk1ODE1YTdlYTEiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vbG9jYWxob3N0IiwiaHR0cHM6Ly8xMDMuMTI2LjE2MS4xOTkiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImRlZmF1bHQtcm9sZXMtbWFzdGVyIiwib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7Im9wZW5yZW1vdGUiOnsicm9sZXMiOlsicmVhZDp1c2VycyIsInJlYWQ6bG9ncyIsInJlYWQ6bWFwIiwicmVhZDpydWxlcyIsInJlYWQ6YXNzZXRzIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6ImVtYWlsIHByb2ZpbGUiLCJzaWQiOiJmNzFlMjVjNi1jOGM4LTRhMTUtODVkYi1lMjk1ODE1YTdlYTEiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsInByZWZlcnJlZF91c2VybmFtZSI6InVzZXIxIn0.PqM4KvK0FmH6CBVfmoRY_LYMw49J6zhZ_O0ZLIeejSjC1Tikn6mRJcCX8SQDrchv0E_xtiAFFy9PA1YikKQ1OaVoysDCyqnBYXstHfGtug6aKww9WF-2-9TwVhCQtjqrZyVmkBcQN-YCejwB06c4m-RcIVjoRov-tesMmsTcIc1APQ24OZiTwOK_UTUK6jmDL0XCfpqtJxjiiiSvJ4h_LCakbtqoWN8QFRWxw1jwNsQZJYHRyNaP3IlhNpoMN9PEe27zhSopKDREp7NCK7RJjJcC8E6qIKjjaBZJqechqoxPT8kaSzDe2ykgPX93gkx7w-dAzo_a53EGrHJEkAHtuQ";
+    private static String token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJLcHRXNWJCcTlsRGliY2s5NHI3TldHQVl0SHBrUFI3N1A4V0hMWDVIX1E0In0.eyJleHAiOjE2NjcxMTE4MzYsImlhdCI6MTY2NzAyNTQzNiwiYXV0aF90aW1lIjoxNjY3MDI1NDM2LCJqdGkiOiI1ZWY5MDA2NC05YTRhLTRlNzAtYTJhMC04YjQ5ZjVlNTMwYmUiLCJpc3MiOiJodHRwczovLzEwMy4xMjYuMTYxLjE5OS9hdXRoL3JlYWxtcy9tYXN0ZXIiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiMTAxZGQ1MmMtMjNiYS00ZjM4LWExMjQtYjc4MGUxYjVhODFiIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoib3BlbnJlbW90ZSIsInNlc3Npb25fc3RhdGUiOiJmYjRlNzUyNy0xNTZiLTQxOTgtOGY2ZS0yYzI5OGU4NWU1NTAiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vbG9jYWxob3N0IiwiaHR0cHM6Ly8xMDMuMTI2LjE2MS4xOTkiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImRlZmF1bHQtcm9sZXMtbWFzdGVyIiwib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7Im9wZW5yZW1vdGUiOnsicm9sZXMiOlsicmVhZDp1c2VycyIsInJlYWQ6bG9ncyIsInJlYWQ6bWFwIiwicmVhZDpydWxlcyIsInJlYWQ6YXNzZXRzIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6ImVtYWlsIHByb2ZpbGUiLCJzaWQiOiJmYjRlNzUyNy0xNTZiLTQxOTgtOGY2ZS0yYzI5OGU4NWU1NTAiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsInByZWZlcnJlZF91c2VybmFtZSI6InVzZXIxIn0.gqhfFSLMQcKMScYBPVomIzp5RgYgbM0Fqln13kVcNlZCiQAh6vfKtE44fc7mAAPWDg0IWMy95-qftK4h5X3GVqZcXsB9MMoTZesDNwZHCiX2ifMaCGxuNA4xCFMB3RtikOI8eZX1sP4UAcUL72afHsBsTtXHSW3jdhG5Bgw0N9KJGutbTXGmCRNtXSiLAh0Db_CxR92vojKkP5dp7Z5Eze5Lk3vG44Ka5LwuEFkq5Fv297cEtKv0l9e2oNEbsdfhZcDGYSflQcOoBcT84QENeLBFm9gJRUAG__nI6lcX3ckWpOLeQgcmr29xbJ18b7KEA-BgBIiJnmx7h9m7ODZ7TQ";
+    private static OkHttpClient getUnsafeOkHttpClient() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new java.security.cert.X509Certificate[]{};
+                        }
+                    }
+            };
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+
+            //Log
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            builder.addInterceptor(interceptor);
+
+            //Bear token
+            builder.addInterceptor(new Interceptor() {
+                @Override
+                public Response intercept(Chain chain) throws IOException {
+                    Request newRequest  = chain.request().newBuilder()
+                            .addHeader("Authorization", "Bearer " + token)
+                            .build();
+                    return chain.proceed(newRequest);
+                }
+            });
+
+
+            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0]);
+            builder.hostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+            OkHttpClient okHttpClient = builder.build();
+            return okHttpClient;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+        static Retrofit getClient() {
+//            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+//            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        /*
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+       OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(Interceptor.Chain chain) throws IOException {
+                Request newRequest  = chain.request().newBuilder()
+                        .addHeader("Authorization", "Bearer " + token)
+                        .build();
+                return chain.proceed(newRequest);
+            }
+        }).build();
+*/
+        OkHttpClient client = getUnsafeOkHttpClient();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://103.126.161.199")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+        return retrofit;
+    }
+}
+
