99716e1ebc41d9645fe2adc8d044032a3540972b
Phuc Nguyen <nguyenphuc2002ok@gmail.com>
2022-11-13 21:52:56 +0700

    render json

--- app/src/main/java/com/example/sampleproject/APIInterface.java
+++ app/src/main/java/com/example/sampleproject/APIInterface.java
@@ -8,8 +8,6 @@ import retrofit2.http.Header;
 import retrofit2.http.Path;
 
 public interface APIInterface {
-
-
     @GET("api/master/asset/{assetID}")
     Call<Asset> getAsset(@Path("assetID") String assetID);//, @Header("Authorization") String auth);
 }
