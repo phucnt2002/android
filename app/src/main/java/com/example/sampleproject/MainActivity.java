0cdad64da9a2dfdf9fade9a3552597a4197bda6c
Phuc Nguyen <nguyenphuc2002ok@gmail.com>
2022-11-28 21:21:01 +0700

    gg map

--- app/src/main/java/com/example/sampleproject/MainActivity.java
+++ app/src/main/java/com/example/sampleproject/MainActivity.java
@@ -23,56 +23,54 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView txttype = (TextView)findViewById(R.id.textView1);
+        setContentView(R.layout.activity_maps);
+//        TextView txttype = (TextView)findViewById(R.id.textView1);
 
-        txttype.setText("HELLO MY FRIEND");
+//        txttype.setText("HELLO MY FRIEND");
         //Call AssetAPI
-        apiInterface = APIClient.getClient().create(APIInterface.class);
-
-//        System.out.println(apiInterface);
-
-//        Call<Asset> call = apiInterface.getAsset("6H4PeKLRMea1L0WsRXXWp9");//, "Bearer "+ token);
-//        call.enqueue(new Callback<Asset>() {
+//        apiInterface = APIClient.getClient().create(APIInterface.class);
+//
+////        System.out.println(apiInterface);
+//
+////        Call<Asset> call = apiInterface.getAsset("6H4PeKLRMea1L0WsRXXWp9");//, "Bearer "+ token);
+////        call.enqueue(new Callback<Asset>() {
+////            @Override
+////            public void onResponse(Call<Asset> call, Response<Asset> response) {
+////                Log.d("API CALL", response.code()+"");
+////                //Log.d ("API CALL", response.toString());
+////                Asset asset = response.body();
+////
+////                Log.d("API CALL", asset.type+"");
+////                txttype.setText(asset.attributes.toString());
+////            }
+////            @Override
+////            public void onFailure(Call<Asset> call, Throwable t) {
+////                Log.d("API CALL", t.getMessage().toString());
+////
+////                //t.printStackTrace();
+////
+////            }
+////        });
+//
+//        //Call mapAPI
+//        mapInterface = APIClient.getClient().create(MapInterface.class);
+//        Call<Map> call = mapInterface.getAllData();
+//        call.enqueue(new Callback<Map>() {
 //            @Override
-//            public void onResponse(Call<Asset> call, Response<Asset> response) {
+//            public void onResponse(Call<Map> call, Response<Map> response) {
 //                Log.d("API CALL", response.code()+"");
 //                //Log.d ("API CALL", response.toString());
-//                Asset asset = response.body();
+//                Map map = response.body();
 //
-//                Log.d("API CALL", asset.type+"");
-//                txttype.setText(asset.attributes.toString());
-//
+//                Log.d("API CALL", map.version+"");
+//                txttype.setText(map.options.toString());
 //            }
 //
 //            @Override
-//            public void onFailure(Call<Asset> call, Throwable t) {
+//            public void onFailure(Call<Map> call, Throwable t) {
 //                Log.d("API CALL", t.getMessage().toString());
-//
-//                //t.printStackTrace();
-//
 //            }
 //        });
-
-        //Call mapAPI
-        mapInterface = APIClient.getClient().create(MapInterface.class);
-        Call<Map> call = mapInterface.getAllData();
-        call.enqueue(new Callback<Map>() {
-            @Override
-            public void onResponse(Call<Map> call, Response<Map> response) {
-                Log.d("API CALL", response.code()+"");
-                //Log.d ("API CALL", response.toString());
-                Map map = response.body();
-
-                Log.d("API CALL", map.version+"");
-                txttype.setText(map.options.toString());
-            }
-
-            @Override
-            public void onFailure(Call<Map> call, Throwable t) {
-                Log.d("API CALL", t.getMessage().toString());
-            }
-        });
 
     }
 }
